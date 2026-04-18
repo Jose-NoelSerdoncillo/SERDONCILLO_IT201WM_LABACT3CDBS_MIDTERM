4730ea983260e417f8b45271ddd2d3bf5b1e8424
JunoCode <Junoserdoncillo43@gmail.com>
2026-04-18 22:59:50 +0800

    Add project files.

---
+++ SERDONCILLO_IT201WM_LABACT3_MIDTERM/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace SERDONCILLO_IT201WM_LABACT3_MIDTERM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Budget = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Cost = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.urgencylevel = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Importance = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Risk = new System.Windows.Forms.ComboBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Importance)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(314, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LIFE DECISION SUPPORT SYSTEM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(332, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BUDGET :";
+            // 
+            // Budget
+            // 
+            this.Budget.Location = new System.Drawing.Point(408, 136);
+            this.Budget.Name = "Budget";
+            this.Budget.Size = new System.Drawing.Size(177, 20);
+            this.Budget.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(271, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ESTIMATED COST :";
+            // 
+            // Cost
+            // 
+            this.Cost.Location = new System.Drawing.Point(408, 177);
+            this.Cost.Name = "Cost";
+            this.Cost.Size = new System.Drawing.Size(177, 20);
+            this.Cost.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(278, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "URGENCY LEVEL :";
+            // 
+            // urgencylevel
+            // 
+            this.urgencylevel.FormattingEnabled = true;
+            this.urgencylevel.Items.AddRange(new object[] {
+            "Low",
+            "Medium",
+            "High"});
+            this.urgencylevel.Location = new System.Drawing.Point(408, 220);
+            this.urgencylevel.Name = "urgencylevel";
+            this.urgencylevel.Size = new System.Drawing.Size(167, 21);
+            this.urgencylevel.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(264, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "IMPORTANCE (1-10) :";
+            // 
+            // Importance
+            // 
+            this.Importance.Location = new System.Drawing.Point(408, 257);
+            this.Importance.Name = "Importance";
+            this.Importance.Size = new System.Drawing.Size(167, 20);
+            this.Importance.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(315, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "RISK LEVEL: ";
+            // 
+            // Risk
+            // 
+            this.Risk.FormattingEnabled = true;
+            this.Risk.Items.AddRange(new object[] {
+            "Low ",
+            "Medium ",
+            "High"});
+            this.Risk.Location = new System.Drawing.Point(408, 297);
+            this.Risk.Name = "Risk";
+            this.Risk.Size = new System.Drawing.Size(167, 21);
+            this.Risk.TabIndex = 10;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(300, 363);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(127, 50);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "RESET FORM ";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(557, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 50);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "EVALUATE DECISION";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(278, 437);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(422, 109);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Label Result";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 710);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.Risk);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Importance);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.urgencylevel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Cost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Budget);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Importance)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Budget;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Cost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox urgencylevel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown Importance;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox Risk;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SERDONCILLO_IT201WM_LABACT3_MIDTERM/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SERDONCILLO_IT201WM_LABACT3_MIDTERM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string CheckIfEmpty(string text)
+        {
+            if (text == "") return "Empty";
+            return "Ok";
+        }
+
+        string CheckIfNumber(string text)
+        {
+            double result;
+            if (double.TryParse(text, out result)) return "Ok";
+            return "Not a Number";
+        }
+
+        string CheckAffordability(double budget, double cost)
+        {
+            if (cost > budget) return "Not Affordable"; 
+            if (cost >= budget * 0.8) return "Critical"; 
+            return "Affordable"; 
+        }
+
+        double ComputePriority(int importance, string urgency)
+        {
+            double weight = 1.0;
+            if (urgency == "High") weight = 2.0; 
+            else if (urgency == "Medium") weight = 1.0;
+            else weight = 0.5;
+
+            return importance * weight; 
+        }
+        string EvaluateRisk(string riskLevel)
+        {
+            return riskLevel; // Simply returns Safe, Moderate, or High Risk [cite: 61, 62]
+        }
+
+        string GenerateDecision(string afford, double score, string risk)
+        {
+            if (afford == "Not Affordable" || risk == "High Risk") return "Not Recommended";
+            if (score >= 10 && afford == "Affordable") return "Proceed Immediately"; 
+            if (score < 5) return "Delay Decision"; 
+            return "Proceed with Caution"; 
+        }
+
+        string GenerateExplanation(string decision)
+        {
+            if (decision == "Proceed Immediately") return "Everything looks good and within budget."; 
+            if (decision == "Not Recommended") return "Too expensive or too much risk involved.";
+            return "Take your time and review your options.";
+        }
+
+        void DisplayResult(string aff, double score, string risk, string dec, string expl)
+        {
+            label7.Text = "AFFORDABILITY: " + aff + "\n" + "PRIORITY SCORE: " + score + "\n" + "RISK LEVEL: " + risk + "\n" + "FINAL DECISION: " + dec + "\n\n" + "NOTE: " + expl; 
+        }
+
+        void ResetForm()
+        {
+            Budget.Text = ""; 
+            Cost.Text = "";
+            urgencylevel.Text = "";
+            Risk.Text = "";
+            Importance.Value = 1;   
+            label7.Text = "Results cleared.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Basic Validation using our functions 
+            if (CheckIfEmpty(Budget.Text) == "Yes" || CheckIfNumber(Budget.Text) == "No")
+            {
+                MessageBox.Show("Please check your Budget input!");
+                return;
+            }
+
+            // Convert data from inputs 
+            double budgetValue = double.Parse(Budget.Text);
+            double costValue = double.Parse(Cost.Text);
+            int importanceValue = (int)Importance.Value;
+            string urgencyValue = urgencylevel.Text;
+            string riskValue = Risk.Text;
+
+            // Call logic functions 
+            string affResult = CheckAffordability(budgetValue, costValue);
+            double prioScore = ComputePriority(importanceValue, urgencyValue);
+            string riskResult = EvaluateRisk(riskValue);
+            string finalDec = GenerateDecision(affResult, prioScore, riskResult);
+            string finalExpl = GenerateExplanation(finalDec);
+
+            // Output everything
+            DisplayResult(affResult, prioScore, riskResult, finalDec, finalExpl);
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+    }
+}
